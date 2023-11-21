2bda2ef5a9394c9b2691f3662c3ae3ee67988347
Erwin Sandoval <53550285+Erwins14@users.noreply.github.com>
2023-11-20 23:03:14 -0600

    Ejemplo escritura Firebase ESP8266 - Arduino

--- escritura_firebase/escritura_firebase.ino
+++ escritura_firebase/escritura_firebase.ino
@@ -6,14 +6,14 @@
 #include <MFRC522.h>
 
 // Configura tu red WiFi y los datos de Firebase
-#define DATABASE_URL "esp8266-ceb36-default-rtdb.firebaseio.com"
-#define API_KEY "T9M0ulw0u5YL3fDzGg20iK9P1mGUOcm4qDxH3Wth"
-#define WIFI_SSID "CLARO_rjNxKd"
-#define WIFI_PASSWORD "C22DCDCC1D"
+#define DATABASE_URL "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define API_KEY "xxxxxxxxxxxxxxxxxxxxxxx"
+#define WIFI_SSID "xxxxxxxxxxxxxxxx"
+#define WIFI_PASSWORD "xxxxxxxxxxxxxxxx"
 
 /* Define el correo electrónico y la contraseña del usuario que ya se ha registrado en tu proyecto */
-#define USER_EMAIL "erwins1414@gmail.com"
-#define USER_PASSWORD "UMG2023"
+#define USER_EMAIL "xxxxxxxxxxxxxxx"
+#define USER_PASSWORD "xxxxxxxxxxxxx"
 
 /* Define la estructura de configuración de Firebase */
 FirebaseConfig config;
